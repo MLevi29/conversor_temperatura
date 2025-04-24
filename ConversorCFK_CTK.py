ca3ee84f3ca4f1c2bc99197ed8ecc29373b4f572
Mateus Levi <mateusmlmg@gmail.com>
2025-04-24 12:23:36 -0300

    Finalização do programa (funcional).

--- ConversorCFK_CTK.py
+++ ConversorCFK_CTK.py
@@ -21,11 +21,11 @@ class App(ctk.CTk):
         self.temperatura.pack(pady=10)
 
         self.texto1 = ctk.StringVar(value=0)
-        self.temp1_valor = ctk.CTkEntry(self, textvariable=self.texto1)
+        self.temp1_valor = ctk.CTkEntry(self, textvariable=self.texto1, width=160)
         self.temp1_valor.bind("<KeyRelease>", self.converter1)
         self.temp1_valor.pack()
         
-        self.temp1 = ctk.CTkOptionMenu(self,
+        self.temp1 = ctk.CTkOptionMenu(self, width=160,
                         values=["Graus Celsius (°C)", "Graus Fahrenheit (°F)", "Kelvin (K)"], command=self.converter2)
         self.temp1.pack()
 
@@ -35,11 +35,11 @@ class App(ctk.CTk):
 
         # Temperatura de saída
         self.texto2 = ctk.StringVar(value=0)
-        self.temp2_valor = ctk.CTkEntry(self, textvariable=self.texto2)
+        self.temp2_valor = ctk.CTkEntry(self, textvariable=self.texto2, width=160)
         self.temp2_valor.bind("<KeyRelease>", self.converter2)
         self.temp2_valor.pack(pady=10)
 
-        self.temp2 = ctk.CTkOptionMenu(self,
+        self.temp2 = ctk.CTkOptionMenu(self, width=160,
                         values=["Graus Celsius (°C)", "Graus Fahrenheit (°F)", "Kelvin (K)"], command=self.converter1)
         self.temp2.pack()
 
@@ -47,73 +47,94 @@ class App(ctk.CTk):
         self.sair = ctk.CTkButton(self, text='Sair', command=self.quit)
         self.sair.pack(pady=5)
             
+
     def converter1(self, event):
         '''Recebe os valores da primeira temperatura e coloca o valor convertido na segunda temperatura.
         Também muda o valor da primeira temperatura caso mude a unidade de medida dela.'''
         if self.temp1_valor.get()=="":
-            self.texto1.set(0)
-
+            self.temp1_valor.configure(text_color='white')
+            pass
         else:
-            entrada = float(self.temp1_valor.get())
+            try:
+                entrada = float(self.temp1_valor.get())
 
-            lista = ["Graus Celsius (°C)", "Graus Fahrenheit (°F)", "Kelvin (K)"]
-            t1 = lista.index(self.temp1.get())
-            t2 = lista.index(self.temp2.get())
-
-            if t1==t2:
-                self.texto2.set(entrada)
+            except ValueError:
+                self.temp1_valor.configure(text_color='red')
+                pass
 
             else:
-                if t1==0:
-                    if t2 == 1:
-                        self.texto2.set(round(round(entrada*1.8+32, 2), 2))
-                    else:
-                        self.texto2.set(round(entrada+273, 2))
+                self.temp1_valor.configure(text_color='white')
+                self.temp2_valor.configure(text_color="white")
 
-                elif t1 == 1:
-                    if t2 == 0:
-                        self.texto2.set(round((entrada-32)/1.8,2))
-                    else:
-                        self.texto2.set(round(((entrada-32)*5/9)+273, 2))
-                elif t1==2:
-                    if t2==0:
-                        self.texto2.set(round(entrada-273,2))
-                    else:
-                        self.texto2.set(round((entrada-273)*1.8+32, 2))
+                lista = ["Graus Celsius (°C)", "Graus Fahrenheit (°F)", "Kelvin (K)"]
+                t1 = lista.index(self.temp1.get())
+                t2 = lista.index(self.temp2.get())
+
+                if t1==t2:
+                    self.texto2.set(round(entrada, 2))
+
+                else:
+                    if t1==0:
+                        if t2 == 1:
+                            self.texto2.set(round(round(entrada*1.8+32, 2), 2))
+                        else:
+                            self.texto2.set(round(entrada+273, 2))
+
+                    elif t1 == 1:
+                        if t2 == 0:
+                            self.texto2.set(round((entrada-32)/1.8,2))
+                        else:
+                            self.texto2.set(round(((entrada-32)*5/9)+273, 2))
+                    elif t1==2:
+                        if t2==0:
+                            self.texto2.set(round(entrada-273,2))
+                        else:
+                            self.texto2.set(round((entrada-273)*1.8+32, 2))
+
 
     def converter2(self, event):
         '''Recebe os valores da segunda temperatura e coloca o valor convertido na primeira temperatura.
         Também muda o valor da segunda temperatura caso mude a unidade de medida dela.'''
         if self.temp2_valor.get()=="":
-            self.texto2.set(0)
+            self.temp2_valor.configure(text_color="white")
+            pass
 
         else:
-            entrada = float(self.temp2_valor.get())
+            try:
+                entrada = float(self.temp2_valor.get())
 
-            lista = ["Graus Celsius (°C)", "Graus Fahrenheit (°F)", "Kelvin (K)"]
-            t1 = lista.index(self.temp1.get())
-            t2 = lista.index(self.temp2.get())
-
-            if t2==t1:
-                self.texto1.set(entrada)
-
+            except ValueError:
+                self.temp2_valor.configure(text_color="red")
+                pass
+            
             else:
-                if t2==0:
-                    if t1 == 1:
-                        self.texto1.set(round(round(entrada*1.8+32, 2), 2))
-                    else:
-                        self.texto1.set(round(entrada+273, 2))
+                self.temp2_valor.configure(text_color="white")
+                self.temp1_valor.configure(text_color="white")
 
-                elif t2 == 1:
-                    if t1 == 0:
-                        self.texto1.set(round((entrada-32)/1.8,2))
-                    else:
-                        self.texto1.set(round(((entrada-32)*5/9)+273, 2))
-                elif t2==2:
-                    if t1==0:
-                        self.texto1.set(round(entrada-273,2))
-                    else:
-                        self.texto1.set(round((entrada-273)*1.8+32, 2))
+                lista = ["Graus Celsius (°C)", "Graus Fahrenheit (°F)", "Kelvin (K)"]
+                t1 = lista.index(self.temp1.get())
+                t2 = lista.index(self.temp2.get())
+
+                if t2==t1:
+                    self.texto1.set(round(entrada, 2))
+
+                else:
+                    if t2==0:
+                        if t1 == 1:
+                            self.texto1.set(round(round(entrada*1.8+32, 2), 2))
+                        else:
+                            self.texto1.set(round(entrada+273, 2))
+
+                    elif t2 == 1:
+                        if t1 == 0:
+                            self.texto1.set(round((entrada-32)/1.8,2))
+                        else:
+                            self.texto1.set(round(((entrada-32)*5/9)+273, 2))
+                    elif t2==2:
+                        if t1==0:
+                            self.texto1.set(round(entrada-273,2))
+                        else:
+                            self.texto1.set(round((entrada-273)*1.8+32, 2))
 
 
 conversor = App()
--- Readme.md
+++ Readme.md
@@ -6,7 +6,7 @@ O presente arquivo apresenta um projeto particular de conversor de temperatura f
 ## Objetivo: 
 Resolvi fazer esse projeto para rever e aprimorar minhas habilidades em programação, além de conhecer mais a fundo a biblioteca Custom Tkinter.
 
-## O que é um conversor de termperatura CFK?
+## O que é um conversor de temperatura CFK?
 O nome "conversor de temperatura" é suficiente. CFK se refere as temperaturas Celsius (°C), Fahrenheit (°F) e Kelvin (K).
 A medida Celsius é a mais usada; Kelvin é uma medida científica; e Fahrenheit é mais utilizada nos EUA. Elas são converdidas pelas seguintes fórmulas:
 
@@ -19,7 +19,22 @@ K = temperatura em kelvin
 
 ## Detalhes:
 O primeiro modelo desse programa foi feito apenas com uma interface simples de terminal e sem orientação a objeto. A única coisa que precisei fazer a mais foi pesquisar as fórmulas de conversão novamente. Nesse ponto, o aprendizado foi ficar mais atento com as excessões, mas o programa foi rápido de fazer. Veja o post no LinkedIn referente a ele [aqui](https://www.linkedin.com/posts/mateusmagalhaes_python-programaaexaeto-activity-7310782971374927872-E5eY?utm_source=share&utm_medium=member_desktop&rcm=ACoAACexfIABu0_IIQtlHmH2oveguEC-MuBYeqA).  
-Atualmente, estou tentando implementar uma interface utilizando OOP. Para isso, tive que mudar a estrutura e transformei a conversão em função. Estou tentando me basear no modelo do conversor do Google, onde não preciso apertar o botão "calcular". Nesse modelo, preciso apenas selecionar as unidades de medida da temperatura e, ao colocar o valor de entrada, o valor de saída aparece automaticamente. Esse tem sido meu desafio atual, pois a interface em si já está estruturada. Veja arquivo "ConversorCFK_CTK.py".
+
+O arquivo "ConversorCFT_CTK.py" é o programa com interface gráfica e totalmente funcional. Além de fazer a conversão, ele também tem um tratamento de erro onde, caso o usuário escreva algo que não possa ser identificado como um número, o programa não faz a conversão e muda a cor do texto para vermelho.  
+
+## Observações e melhorias
+Podemos dizer que o programa em si está completo, ou seja, está funcionando sem erros e cumpre seu propósito. Entretanto, podemos melhorá-lo.
+
+- A escala Kelvin não tem números negativos, mas o programa permite que isso ocorra.  
+- Apesar de eu ter posto um arredondamento com duas casas decimais nas respostas, você consegue escrever um número com mais casas. Não vejo problema nisso visto que a resposta ainda terá duas casas decimais - mas pode ser uma melhoria.  
+- Colocar um ícone informativo que explique minimamente o funcionamento do programa ou a conversão pode ser interessante.  
+- Mudar os ícones da janela e do programa pode ser interessante, ao invés de deixar as imagens padrões do Custom Tkinter e do python.  
+- Precisei usar duas funções na conversão. Será que tem como simplificar e colocar uma única função sem comprometer a legibilidade?  
+- Eu não mudei muito o estilo da interface pois meu objetivo não era criar um app bonito, mas sim funcional.  
+- O aplicativo não está no formato executável. Farei isso assim que puder.  
+
+## Conclusão
+Apesar das observações acima, o aprendizado foi muito bom. Entendi melhor o processo de criação de uma interface gráfica com uma estrutura de OOP e me sinto preparado para criar outros aplicativos.
 
 ## Referência
 
